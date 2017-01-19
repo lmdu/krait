3e46b0cf267befd51e9f571e568cea5ccba31549
Lianming Du <adullb@qq.com>
2017-01-19 22:51:34 +0800

    changed the motif standard methods

--- motif.py
+++ motif.py
@@ -2,105 +2,152 @@
 # -*- coding: utf-8 -*-
 import itertools
 
-def isMiniMotif(motif):
+def is_motif(motif):
 	'''
 	check the motif length is weather or not minimal length,
-	e.g. AAAA is mono- motif A not tetra- motif.
-	@motif string.
-	@return bool.
+	e.g. AAAA is mono- motif A not tetra- motif
+	@motif string
+	@return bool
 	'''
-	if len(set(motif)) == 1: return True
-	
-	if len(motif) % 2 != 0: return False
-	
+	if len(motif) == 1:
+		return True
+
+	if len(set(motif)) == 1:
+		return False
+
+	if len(motif) in (3,5):
+		return True
+
+	if len(motif) == 2:
+		if motif[0] == motif[-1]:
+			return False
+		else:
+			return True
+
 	if len(motif) == 4:
-		return True if motif[0:2] == motif[2:4] else False
+		if motif[0:2] == motif[2:4]:
+			return False
+		else:
+			return True
 
 	if len(motif) == 6:
 		if motif[0:3] == motif[3:6]:
-			return True
-		elif motif[0:2] == motif[2:4] and motif[2:4] == motif[4:6]:
-			return True
-		else:
 			return False
 
-def getSimilarMotif(motif):
+		elif motif[0:2] == motif[2:4] == motif[4:6]:
+			return False
+
+		else:
+			return True
+
+def similar_motif(motif):
 	'''
 	remove the similarity motifs e.g. AC and CA, CA is AC.
 	@motif string.
 	@return string, return the first motif.
 	'''
-	if len(motif) == 2: return motif[::-1]
+	if len(motif) == 1:
+		return [motif]
+	
 	similar_motifs = []
 	for i, b in enumerate(motif):
 		new_motif = "%s%s" % (motif[i+1:], motif[0:i+1])
 		similar_motifs.append(new_motif)
-		if new_motif == motif: break
-	similar_motifs.sort()
-	return similar_motifs[0]
+	return similar_motifs
 
-def reverseCompleteMotif(motif):
+def complete_motif(motif):
 	'''
-	remove the reverse complete motifs e.g. AC and GT.
-	@motif string.
-	@return string, the reversed and completed motif.
+	motif completement eg. TG -> AG
+	@para motif str
+	@return list
 	'''
 	codes = {'A': 'T', 'T': 'A', 'G': 'C', 'C': 'G'}
-	return "".join([ codes[c] for c in motif[::-1]])
+	new_motif = "".join([ codes[base] for base in motif])
+	return similar_motif(new_motif)
 
-def generateMotifTable(mlen):
+
+def reverse_complete_motif(motif):
 	'''
-	generate all motifs by motif length.
-	@mlen integer, motif length 2~7.
-	@return dict, the motifs table.
+	remove the reverse complete motifs e.g. AC and GT.
+	@motif string
+	@return list
 	'''
-	bases = ['A', 'C', 'G', 'T']
-	table = {}
-	for motif in itertools.product(bases, repeat=mlen):
-		motif = "".join(list(motif))
+	return complete_motif(motif[::-1])
+
+def reverse_motif(motif):
+	return similar_motif(motif[::-1])
+
+def motif_to_number(motif):
+	sort_rule = {'A':'1', 'T':'2', 'C':'3', 'G':'4'}
+	return int("".join(sort_rule[a] for a in motif.upper()))
+
+def motif_sorted(motifs):
+	return sorted(motifs, key=motif_to_number)
+
+
+class StandardMotif:
+	_motifs = {}
+	def __init__(self, level=0):
+		self.level = level
+
+	def setLevel(self, level=0):
+		self._motifs = {}
+		self.level = level
+
+	def standard(self, motif):
+		if self.level == 0:
+			return motif
+
+		if motif in self._motifs:
+			return self._motifs[motif]
+
+		motifs = []
 		
-		if isMiniMotif(motif): continue
-		
-		new_motif = getSimilarMotif(motif)
-		if new_motif in table:
-			table[new_motif].append(motif)
-			continue
+		if self.level >= 1:
+			motifs.extend(similar_motif(motif))
 
-		new_motif = reverseCompleteMotif(new_motif)
-		if new_motif in table:
-			table[new_motif].append(motif)
-			continue
+		if self.level >= 2:
+			motifs.extend(reverse_complete_motif(motif))
 
-		new_motif = reverseCompleteMotif(motif)
-		if new_motif in table:
-			table[new_motif].append(motif)
-			continue
+		if self.level >= 3:
+			motifs.extend(complete_motif(motif))
 
-		new_motif = getSimilarMotif(new_motif)
-		if new_motif in table:
-			table[new_motif].append(motif)
-			continue
+		if self.level >= 4:
+			motifs.extend(reverse_motif(motif))
 
-		table[motif] = []
-	
-	return table
 
-MOTIFS = {'A': 'A', 'C': 'C', 'T':'A', 'G':'C'}
+		#remove the same motifs
+		motifs = list(set(motifs))
 
-#generate the all motifs 1~6.
-for i in range(2, 7):
-	motifs = generateMotifTable(i)
-	for m1 in motifs:
-		MOTIFS[m1] = m1
-		for m2 in motifs[m1]:
-			MOTIFS[m2] = m1
+		#sort motifs as A>T>C>G
+		motifs = motif_sorted(motifs)
 
-def normalize(motif):
-	return MOTIFS[motif]
+		for motif in motifs:
+			self._motifs[motif] = motifs[0]
 
+		return motifs[0]
+
+	def mapping(self):
+		bases = ['A', 'T', 'C', 'G']
+		motifs = {}
+		for i in range(6):
+			for motif in itertools.product(bases, repeat=i+1):
+				motif = "".join(list(motif))
+				if not is_motif(motif):
+					continue
+
+				smotif = self.standard(motif)
+				if smotif not in motifs:
+					motifs[smotif] = []
+
+				if motif not in motifs[smotif]:
+					motifs[smotif].append(motif)
+
+		return motifs
 
 
 if __name__ == '__main__':
-	import json
-	with open("motif.json", "w") as fp:
-		json.dump(MOTIFS, fp, indent=4)
+	s = StandardMotif(level=4)
+	motifs = s.mapping()
+	for motif in sorted(motifs.keys(), key=lambda x: (len(x), motif_to_number(x))):
+		print "\t".join(motif_sorted(motifs[motif]))
