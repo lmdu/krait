cd1e6ddbf8038c7f65357ec42eaa31b9ddf3f1d6
Lianming Du <adullb@qq.com>
2017-02-02 23:14:53 +0800

    add statistics module

--- statistics.py
+++ statistics.py
@@ -8,8 +8,10 @@ class Statistics:
 	meta_table = MetaTable()
 
 class SequenceStatistics(Statistics):
-	def __init__(self):
+	def __init__(self, unit='Mb', letter='ATCG'):
 		self.table = FastaTable()
+		self.unit = unit
+		self.letter = letter
 		self._bases = {'A':0,'G':0,'C':0,'T':0}
 		self._total_sequences = 0
 		self._total_bases = 0
@@ -33,8 +35,6 @@ class SequenceStatistics(Statistics):
 	def _db_bases(self):
 		for data in self.meta_table.query("SELECT * FROM meta WHERE name IN ('A', 'T', 'G', 'C')"):
 			self._bases[data.name] = data.value
-
-
 
 	@property
 	def bases(self):
@@ -64,23 +64,62 @@ class SequenceStatistics(Statistics):
 		return self._total_bases
 
 	def getNs(self):
-		return self.total_bases - self.getValidSize()
+		return self._total_bases - self.getValidSize()
 
 	def getValidSize(self):
 		'''
 		get sequence valid total length A+G+T+C
 		'''
-		return sum(self._bases.values())
+		return sum(self.bases.values())
 
 	def getGCContent(self):
-		gc = self._bases['G'] + self._bases['C']
+		gc = self.bases['G'] + self.bases['C']
 		return round(gc/float(self.getValidSize()), 2)
 
+	def getScaleSize(self):
+		scales = {'Mb': 1000000.0, 'Kb': 1000.0}
+		if self.letter == 'ATGC':
+			total = self.getValidSize()
+
+		elif self.letter == 'ATGCN':
+			total = self.getSize()
+
+		return total/scales[self.unit]
+
 	def getRelativeAbundance(self, counts):
-		pass
+		return round(counts/self.getScaleSize(), 2)
 		
 	def getRelativeDensity(self, lengths):
+		return round(lengths/self.getScaleSize(), 2)
+
+class MicrosatelliteStatistics(Statistics):
+	def __init__(self):
+		self.ssr_table = MicrosatelliteTable()
+
+	@property
+	def counts(self):
+		return self.ssr_table.recordCounts()
+
+	@property
+	def lengths(self):
+		return self.ssr_table.get("SELECT SUM(length) FROM ssr")
+
+	def motifLength(self):
+		sql = "SELECT length(motif) AS type, COUNT(1) AS count FROM ssr GROUP BY type"
+		return {row.type: row.count for row in self.ssr_table.query(sql)}
+
+	def motifType(self):
+		sql = "SELECT standard, COUNT(1) AS count FROM ssr GROUP BY standard"
+		return {row.standard: row.count for row in self.ssr_table.query(sql)}
+
+	def motifRepeat(self):
+		sql = "SELECT repeat, COUNT(1) AS count FROM ssr GROUP BY repeat"
+		return {row.repeat: row.count for row in self.ssr_table.query(sql)}
+
+	def report(self):
 		pass
+
+		
 
 
 
