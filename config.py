e926bcfc18bb9406361234e099dc9ce6dd28eacf
Lianming Du <adullb@qq.com>
2017-07-28 15:25:24 +0800

    changed version

--- config.py
+++ config.py
@@ -3,9 +3,9 @@
 import os
 from jinja2 import Environment, FileSystemLoader
 
-VERSION = '1.0.0'
+VERSION = '0.8.1'
 
-BUILD = '20170727'
+BUILD = '20170728'
 
 ROOT_PATH = os.path.abspath(os.path.dirname(__file__))
 
