427ca637525b53f51928f3382d4c9dc04054e1fb
Lianming Du <adullb@qq.com>
2022-12-24 22:56:26 +0800

    fixed ci

--- ci/fix_compiler_error.py
+++ ci/fix_compiler_error.py
@@ -1,14 +1,11 @@
 import os
 import sys
 
-filename = os.path.join(sys.argv[1], r'Lib\distutils\cygwinccompiler.py')
+filename = os.path.join(sys.exec_prefix, 'Lib', 'distutils', 'cygwinccompiler.py')
+target = 'raise ValueError("Unknown MS Compiler version %s " % msc_ver)'
 
 with open(filename) as fp:
-	if sys.version_info[0:2] > (3, 5):
-		lines = fp.read().replace("            return ['msvcr100']", "            return ['msvcr100']\n        elif msc_ver == '1916':\n            return ['vcruntime140']")
-
-	else:
-		lines = fp.read().replace("            return ['msvcr100']", "            return ['msvcr100']\n        elif msc_ver == '1900':\n            return ['vcruntime140']")
+	lines = fp.read().replace(target, 'return ['vcruntime140']')
 
 with open(filename, "w") as fw:
 	fw.write(lines)
