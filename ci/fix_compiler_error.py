41df9e715b0f6f646cbc3af56d950d5c929525fe
Lianming Du <adullb@qq.com>
2022-12-24 22:58:58 +0800

    Update fix_compiler_error.py

--- ci/fix_compiler_error.py
+++ ci/fix_compiler_error.py
@@ -5,7 +5,7 @@ filename = os.path.join(sys.exec_prefix, 'Lib', 'distutils', 'cygwinccompiler.py
 target = 'raise ValueError("Unknown MS Compiler version %s " % msc_ver)'
 
 with open(filename) as fp:
-	lines = fp.read().replace(target, 'return ['vcruntime140']')
+	lines = fp.read().replace(target, 'return ["vcruntime140"]')
 
 with open(filename, "w") as fw:
 	fw.write(lines)
